0e7f06e1d0a996a0652e7a80bcc7f2239bf8375b
Dishan <dishanf@irononetech.com>
2022-12-29 16:23:48 +0530

    Solution: Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun processInput(input: String): List<List<Int>> {
+        return input
+            .split("\n\n")
+            .map { elf -> elf.lines().map { it.toInt() } }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val calories = processInput(input)
+        return calories.maxOf { it.sum() }
+    }
+
+    fun part2(input: String): Int {
+        val calories = processInput(input)
+        return calories.map { it.sum() }.sorted().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputText("Day01_test")
+    check(part1(testInput) == 30)
+    check(part2(testInput) == 75)
 
-    val input = readInput("Day01")
+    val input = readInputText("Day01")
     part1(input).println()
     part2(input).println()
 }
